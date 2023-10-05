d4c1212126026c5d79416fc99ac1890677eb779a
EdPaillard <ed.paillard@gmail.com>
2023-10-05 16:30:28 +0200

    url fix

--- src/services/FragmentsService.tsx
+++ src/services/FragmentsService.tsx
@@ -11,7 +11,7 @@ export async function getFragSample(): Promise<Array<Fragment>> {
     const url = process.env.REACT_APP_API_URL
     console.log(url)
 
-    const response: AxiosResponse<Array<Fragment>> = await axios.get(`${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/fragment/sample`)
+    const response: AxiosResponse<Array<Fragment>> = await axios.get(`http://${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/fragment/sample`)
     const datas = response.data
 
     return datas;
@@ -24,7 +24,7 @@ export async function listFrags(): Promise<Array<Fragment>> {
     // })
     // axios.defaults.httpsAgent = agent;
 
-    const response: AxiosResponse<Array<Fragment>> = await axios.get(`${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/fragment`)
+    const response: AxiosResponse<Array<Fragment>> = await axios.get(`http://${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/fragment`)
     const datas = response.data
 
     return datas;
@@ -37,7 +37,7 @@ export async function getFragByID(id: string): Promise<Fragment> {
     // })
     // axios.defaults.httpsAgent = agent;
     console.log(id)
-    const response: AxiosResponse<Fragment> = await axios.get(`${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/fragment/meta/${id}`)
+    const response: AxiosResponse<Fragment> = await axios.get(`http://${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/fragment/meta/${id}`)
     const datas = response.data
 
     return datas;
@@ -53,7 +53,7 @@ export async function createMeta(meta: any, token: string): Promise<number> {
         "fragment" : meta
     }
 
-    const response: AxiosResponse<Fragment> = await axios.post(`${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/fragment`, body, {headers: {
+    const response: AxiosResponse<Fragment> = await axios.post(`http://${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/fragment`, body, {headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`
     },
@@ -76,7 +76,7 @@ export async function createContent(content: any, id: number, type: string, toke
         "type": type
     }
 
-    const response: AxiosResponse<string> = await axios.post(`${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/content`, body, {headers: {
+    const response: AxiosResponse<string> = await axios.post(`http://${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/content`, body, {headers: {
         'Content-Type': 'multipart/form-data',
         'Authorization': `Bearer ${token}`
     },
@@ -98,7 +98,7 @@ export async function createMini(mini: any, id: number, token: string): Promise<
         "id": id
     }
 
-    const response: AxiosResponse<string> = await axios.post(`${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/miniature`, body, {headers: {
+    const response: AxiosResponse<string> = await axios.post(`http://${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/miniature`, body, {headers: {
         'Content-Type': 'multipart/form-data',
         'Authorization': `Bearer ${token}`
     },
